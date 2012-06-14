e4aeb95569da5a729e0d1675c64851e14067cb86
Jason Denizac <jason@denizac.org>
2012-06-14 02:37:41 -0600

    Update paths

--- src/js/main.js
+++ src/js/main.js
@@ -2,10 +2,10 @@
 
 require.config({
 	paths: {
-		"jquery": "lib/jquery-1.7.2.min",
-		"lodash": "lib/lodash-0.3.1.min",
-		"text": "lib/text",
-		"tpl": "lib/tpl",
+		"jquery": "../../lib/jquery-1.7.2.min",
+		"lodash": "../../lib/lodash-0.3.1.min",
+		"text": "../../lib/text",
+		"tpl": "../../lib/tpl",
 		"templates": "../templates"
 	}
 });
